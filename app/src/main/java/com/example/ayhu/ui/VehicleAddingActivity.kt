c0d09c56cb530e31df78cb0a42c71930031df2d4
Muhammed Sevuk <sevukmuhammed@gmail.com>
2020-03-28 23:35:44 +0300

    Button fonksiyonu eklendi

--- app/src/main/java/com/example/ayhu/ui/VehicleAddingActivity.kt
+++ app/src/main/java/com/example/ayhu/ui/VehicleAddingActivity.kt
@@ -8,9 +8,11 @@ import com.example.ayhu.R
 import com.example.ayhu.dto.FuelInfoDTO
 import com.example.ayhu.model.FuelInformation
 import com.example.movieclone.network.RetrofitFactory
+import kotlinx.android.synthetic.main.activity_vehicle_adding.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.content.Intent
 
 
 class VehicleAddingActivity : AppCompatActivity() {
@@ -40,6 +42,7 @@ class VehicleAddingActivity : AppCompatActivity() {
                 }
             }
         })
+        goBackButton()
     }
     fun SystemUIVisibility(){
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -48,5 +51,11 @@ class VehicleAddingActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+    fun goBackButton(){
+        goBack1.setOnClickListener {
+            var intent:Intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
