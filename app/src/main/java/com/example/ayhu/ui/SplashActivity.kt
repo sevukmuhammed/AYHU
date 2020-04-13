974a9384a93f329133ff1c90bd9873c6182922a0
Muhammed Sevuk <sevukmuhammed@gmail.com>
2020-04-13 21:30:08 +0300

    splash screen added

---
+++ app/src/main/java/com/example/ayhu/ui/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.example.ayhu.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import com.example.ayhu.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_vehicle_adding.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        setContentView(R.layout.activity_splash)
+        val a = AnimationUtils.loadAnimation(this, R.anim.fade)
+        imageView.startAnimation(a)
+        textView.startAnimation(a)
+        val i = Intent(this, MainActivity::class.java)
+        val timer = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(3000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(i)
+                    finish()
+                }
+            }
+        }
+        timer.start()
+    }
+}
