f218cf1bb6ab252e439b532ae21e0f3149b54b95
uygemre <46091143+uygemre@users.noreply.github.com>
2020-04-13 14:34:00 +0300

    onboarding page added

--- app/src/main/java/com/example/ayhu/dto/FuelInfoDTO.kt
+++ app/src/main/java/com/example/ayhu/dto/FuelInfoDTO.kt
@@ -3,7 +3,7 @@ package com.example.ayhu.dto
 import android.provider.ContactsContract
 import com.example.ayhu.model.BaseModel
 import com.example.ayhu.model.BaseModel.Companion.TYPE_FUEL
-
+//askljdsadgi
 
 data class FuelInfoDTO(
     var benzin: Float,
