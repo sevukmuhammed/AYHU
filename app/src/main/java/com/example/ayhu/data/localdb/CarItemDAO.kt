dc1d336dc30110ad354a2568104cf55c4eb9c63a
Muhammed Sevuk <sevukmuhammed@gmail.com>
2020-05-14 17:11:44 +0300

    Pull

--- app/src/main/java/com/example/ayhu/data/localdb/CarItemDAO.kt
+++ app/src/main/java/com/example/ayhu/data/localdb/CarItemDAO.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.ayhu.data.model.CarItem
+import retrofit2.http.Query
 
 @Dao
 interface CarItemDAO {
