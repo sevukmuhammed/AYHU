84ac6bdc1625797b8c1391b3598c1aa60628ea5a
uygemre <46091143+uygemre@users.noreply.github.com>
2020-05-16 01:06:06 +0300

    onBoarding page bug fixed

--- app/src/main/java/com/example/ayhu/ui/IntroSlideActivity.kt
+++ app/src/main/java/com/example/ayhu/ui/IntroSlideActivity.kt
@@ -70,7 +70,7 @@ class IntroSlideActivity: AppCompatActivity() {
             }
         }
     }
-    // version
+
 
     private fun setupIndicators() {
         val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
