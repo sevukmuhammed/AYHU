fd54d6996b515c5dfe62016399ab4632992121e4
yanalderya <derya.yanal@gmail.com>
2020-05-15 23:54:55 +0300

    maps activity

--- app/src/main/java/com/example/ayhu/ui/MainActivity.kt
+++ app/src/main/java/com/example/ayhu/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.ayhu.R
-import com.example.ayhu.ui.vehicleadding.VehicleAddingActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/ayhu/ui/MapsActivity.kt
+++ app/src/main/java/com/example/ayhu/ui/MapsActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ayhu.ui
 
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.ayhu.R
@@ -15,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
@@ -30,14 +34,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
+
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         //burada gelen lat long değeri default değere atanıyor
         //kullanıocının konumunu alıp buradaki location bilgisini almamız gerekiyor.
         val myPlace = LatLng(40.73, -73.99)  // this is New York
-        map.addMarker(MarkerOptions().position(myPlace).title("My Favorite City"))
+
+
+
         map.moveCamera(CameraUpdateFactory.newLatLng(myPlace))
         map.uiSettings.isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
@@ -70,7 +79,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
             }
         }
-
         // 1
         map.isMyLocationEnabled = true
 
@@ -90,5 +98,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         val markerOptions = MarkerOptions().position(location)
         // 2
         map.addMarker(markerOptions)
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        // 1
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            // 2
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            // 3
+            if (null != addresses && !addresses.isEmpty()) {
+                address = addresses[0]
+                for (i in 0 until address.maxAddressLineIndex) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+        return addressText
     }
 }
